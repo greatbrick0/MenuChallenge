10c389230821183bde2500235f333556732bd602
greatbrick0 <speharley@gmail.com>
2023-10-15 01:59:17 -0400

    Decorated Start Menu

--- MenuChallenge/Assets/Scripts/ButtonType1.cs
+++ MenuChallenge/Assets/Scripts/ButtonType1.cs
@@ -16,11 +16,7 @@ public class ButtonType1 : MonoBehaviour
     [SerializeField]
     Color rimColour;
     [SerializeField]
-    Color rimHighColour;
-    [SerializeField]
-    Color textColour;
-    [SerializeField]
-    Color textHighColour;
+    Color rimHighColour, textColour, textHighColour;
 
     [Header("References")]
     [SerializeField]
@@ -28,19 +24,29 @@ public class ButtonType1 : MonoBehaviour
 
     private void Start()
     {
-        foreach(GameObject ii in rimObjects)
-        {
-            ii.GetComponent<Image>().color = rimColour;
-        }
-        foreach (GameObject ii in textObjects)
-        {
-            ii.GetComponent<TextMeshProUGUI>().color = textColour;
-        }
+        foreach(GameObject ii in rimObjects) ii.GetComponent<Image>().color = rimColour;
+        foreach (GameObject ii in textObjects) ii.GetComponent<TextMeshProUGUI>().color = textColour;
     }
 
     public void Press()
     {
         //print(Random.Range(0, 10));
         if(animator != null) animator.SetTrigger("FileClicked");
+        StartPersonalAnimation();
+    }
+
+    private void StartPersonalAnimation()
+    {
+        foreach (GameObject ii in rimObjects) ii.GetComponent<Image>().color = rimHighColour;
+        foreach (GameObject ii in textObjects) ii.GetComponent<TextMeshProUGUI>().color = textHighColour;
+        LeanTween.cancel(gameObject);
+
+        LeanTween.value(gameObject, 0.01f, 1.0f, 0.8f)
+            .setEaseInOutSine()
+            .setOnUpdate((value) =>
+            {
+                foreach (GameObject ii in rimObjects) ii.GetComponent<Image>().color = Color.Lerp(rimHighColour, rimColour, value);
+                foreach (GameObject ii in textObjects) ii.GetComponent<TextMeshProUGUI>().color = Color.Lerp(textHighColour, textColour, value);
+            });
     }
 }
